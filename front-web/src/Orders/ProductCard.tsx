c0fdc4e0376c2283f3e467a61401209102d32fb2
Alberto Costa <mihcadevs@gmail.com>
2021-01-10 20:21:55 -0300

    finish front web

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,25 +1,21 @@
 import { Product } from './types';
+import {formatPrice} from './helpers'
 
 type Props = {
     product: Product;
-}
-
-function formatPrice(price: number) {
-    const formatter = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    })
-
-    return formatter.format(price);
-
-
+    isSelected:boolean;
+    onSelectProduct: (product : Product) => void;
 }
 
 
-function ProductCard({ product }: Props) {
+
+
+function ProductCard({ product, onSelectProduct, isSelected }: Props) {
     return (
-
-        <div className="order-card-container">
+        <div 
+        className={`order-card-container ${isSelected ? 'selected' : ''}`}
+            onClick={() => onSelectProduct(product)}
+            >
             <h3 className="order-card-title">
                 {product.name}
             </h3>
